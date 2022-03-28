5b72df4bcc16fc8edce5c3c19981752b900cff82
Matúš Tomlein <matus.tomlein@gmail.com>
2022-03-28 17:01:11 +0200

    Initial Commit

---
+++ DemoSnowplowApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DemoSnowplowApp
+//
+//  Created by Matus Tomlein on 28/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
